9d7467680972d31a3a2da3409e42c6cf8879a63b
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-04-20 21:46:33 +0900

    Add コメントを作成

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,11 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
+/**
+ * 環境変数を読み込み返す。
+ * なかった場合、エラーを出力し終了する。
+ * @param key 環境変数の名前
+ */
 export const GetVal = (key: string): string => {
   const val = process.env[key]
   if (!val) throw new Error('環境変数が見つかりませんでした。')
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as bot from './bot'
 const PORT = process.env.PORT || 5000
 
 express()
-  .post('/post/', (req, res) => bot.PostMessage(req, res))
+  .post('/post/', (req, res) => bot.NoticeLine(req, res))
   .post('/hook/', line.middleware(bot.Config), (req, res) => {
     res.status(200).end()
     req.body.events.forEach((e: line.WebhookEvent) => bot.EchoMessage(e))
